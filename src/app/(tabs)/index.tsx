0d1dcac540e5ae1ced85dd4d4b297ce167653ed6
mattiasphbg <mattiasp@hotmail.com>
2024-10-19 17:29:38 +0200

    Refactor: Add Text component and update HomeScreen
    - Added a new Text component to the UI library.
    - Updated the HomeScreen component to use the new Text component for displaying "Home Screen" text.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 import { View } from "react-native";
-
+import { Text } from "src/src/components/ui/Text";
 export default function HomeScreen() {
-  return <View className=""></View>;
+  return (
+    <View className="">
+      <Text className="text-typography-700 font-body text-base">
+        Home Screen
+      </Text>
+    </View>
+  );
 }
