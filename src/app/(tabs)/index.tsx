62d54003874ab735f3c5681f436023e7ac378bd4
mattiasphbg <mattiasp@hotmail.com>
2024-10-20 12:48:50 +0200

    Refactor: Add @expo/html-elements dependency and update package.json

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { View } from "react-native";
 import { Text } from "src/src/components/ui/Text";
 export default function HomeScreen() {
   return (
-    <View className="">
+    <View>
       <Text className="text-typography-700 font-body text-base">
         Home Screen
       </Text>
--- src/components/ui/styles.tsx
+++ src/components/ui/styles.tsx
@@ -45,3 +45,41 @@ export const textStyle = tva({
     },
   },
 });
+
+export const headingStyle = tva({
+  base: `text-typography-900 font-bold font-heading tracking-sm my-0 ${baseStyle}`,
+  variants: {
+    isTruncated: {
+      true: "truncate",
+    },
+    bold: {
+      true: "font-bold",
+    },
+    underline: {
+      true: "underline",
+    },
+    strikeThrough: {
+      true: "line-through",
+    },
+    sub: {
+      true: "text-xs",
+    },
+    italic: {
+      true: "italic",
+    },
+    highlight: {
+      true: "bg-yellow-500",
+    },
+    size: {
+      "5xl": "text-6xl",
+      "4xl": "text-5xl",
+      "3xl": "text-4xl",
+      "2xl": "text-3xl",
+      xl: "text-2xl",
+      lg: "text-xl",
+      md: "text-lg",
+      sm: "text-base",
+      xs: "text-sm",
+    },
+  },
+});
