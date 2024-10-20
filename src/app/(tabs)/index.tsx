22eb97e750656d36589ac0302baeb9db14405b92
mattiasphbg <mattiasp@hotmail.com>
2024-10-20 13:00:37 +0200

    Refactor: Add Heading component to HomeScreen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
 import { View } from "react-native";
 import { Text } from "src/src/components/ui/Text";
+import { Heading } from "src/src/components/ui/Heading";
 export default function HomeScreen() {
   return (
     <View>
+      <Heading className="text-typography-700 font-body text-2xl">
+        Home Screen
+      </Heading>
       <Text className="text-typography-700 font-body text-base">
         Home Screen
       </Text>
