acb8a8b9d26530508853fe68b2c150b34104606a
mattiasphbg <mattiasp@hotmail.com>
2024-10-28 18:08:53 +0100

    refactor: add bun to .gitignore

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
+
 import { View } from "react-native";
 import { Text } from "src/src/components/ui/Text";
 import { Heading } from "src/src/components/ui/Heading";
 export default function HomeScreen() {
+  // 1. Get the user's location
+
   return (
     <View>
       <Heading className="text-typography-700 font-body text-2xl">
         Home Screen
       </Heading>
-      <Text className="text-typography-700 font-body text-base">
-        Home Screen
-      </Text>
     </View>
   );
 }
