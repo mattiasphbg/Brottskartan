b0dd02ac513b04bf2261091bfa30b223fbeb80db
mattiasphbg <mattiasp@hotmail.com>
2024-12-28 19:42:26 +0100

    feat: add react-native-maps dependency and integrate MapView in HomeScreen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { View } from "react-native";
 import { Text } from "src/src/components/ui/Text";
 import { Heading } from "src/src/components/ui/Heading";
+import MapView from "react-native-maps";
 export default function HomeScreen() {
   // 1. Get the user's location
 
@@ -11,6 +12,7 @@ export default function HomeScreen() {
       <Heading className="text-typography-700 font-body text-2xl">
         Home Screen
       </Heading>
+      <MapView style={{ width: "100%", height: "100%" }} />
     </View>
   );
 }
