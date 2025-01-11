8db5dca94cb74c3894ecefb213f3edf8d72c9053
mattiasphbg <mattiasp@hotmail.com>
2025-01-11 11:28:44 +0100

    feat: add markers to MapView in HomeScreen for enhanced location display

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,16 +3,50 @@ import * as React from "react";
 import { View } from "react-native";
 import { Text } from "src/src/components/ui/Text";
 import { Heading } from "src/src/components/ui/Heading";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 export default function HomeScreen() {
-  // 1. Get the user's location
+  const markers = [
+    {
+      id: 1,
+      latitude: 37.78825,
+      longitude: -122.4324,
+      title: "Marker 1",
+      description: "Description for Marker 1",
+    },
+    {
+      id: 2,
+      latitude: 37.75825,
+      longitude: -122.4624,
+      title: "Marker 2",
+      description: "Description for Marker 2",
+    },
+    {
+      id: 3,
+      latitude: 37.76825,
+      longitude: -122.4524,
+      title: "Marker 3",
+      description: "Description for Marker 3",
+    },
+  ];
 
   return (
     <View>
       <Heading className="text-typography-700 font-body text-2xl">
         Home Screen
       </Heading>
-      <MapView style={{ width: "100%", height: "100%" }} />
+      <MapView style={{ width: "100%", height: "100%" }}>
+        {markers.map((marker) => (
+          <Marker
+            key={marker.id}
+            coordinate={{
+              latitude: marker.latitude,
+              longitude: marker.longitude,
+            }}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </MapView>
     </View>
   );
 }
